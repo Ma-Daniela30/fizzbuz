c68f55d5d07d30dc4b7f088dd8439c718ef5548d
Ma-Daniela30 <launchx04441@innovaccion.mx>
2022-05-11 15:11:30 -0500

    Creando la funcion  refactorExplorer

--- app.js
+++ app.js
@@ -72,4 +72,31 @@ const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) =>
   assignFizzBuzzTrick(explorer)
 );
 
-// Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+/* Part 8: Get a list of the explorers in node, 
+if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, 
+if is just divisible by 5 set the property trcik and the value BUZZ, 
+if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO*/
+const refactorExplorer = function (explorer) {
+  if (explorer.score % 5 === 0 && explorer.score % 3 === 0) {
+    explorer.trick = "FIZZBUZZ";
+    return explorer;
+  } else {
+    if (explorer.score % 5 === 0) {
+      explorer.trick = "BUZZ";
+      return explorer;
+    } else {
+      if (explorer.score % 3 === 0) {
+        explorer.trick = "FIZZ";
+        return explorer;
+      } else {
+        explorer.trick = explorer.score;
+        return explorer;
+      }
+    }
+  }
+};
+
+const refactorexplorer = explorersInNode.map((explorer) =>
+  refactorExplorer(explorer)
+);
+console.log(refactorexplorer);
